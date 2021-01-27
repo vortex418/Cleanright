d9ff32e51c5e62a6c4d3cdeff1c1f4bc8d111306
Vortex418 <75621402+vortex418@users.noreply.github.com>
2021-01-27 12:03:30 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById("button-home").addEventListener("click", function(){
+	window.location.assign("/cleanright/");
+}, false);
+document.getElementById("button-contact").addEventListener("click", function(){
+	window.location.assign("/cleanright/contact.html");
+}, false);
+document.getElementById("button-about").addEventListener("click", function(){
+	window.location.assign("/cleanright/about.html");
+}, false);
